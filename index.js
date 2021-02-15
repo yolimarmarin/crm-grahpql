8384e9a6d20c42e16559b53c68e3686491d502b2
yolimarmarin <yolimar.ma83@gmail.com>
2021-02-15 18:24:23 -0500

    .

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = new ApolloServer({
 });
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 
 server.applyMiddleware({ app, path: "/", cors: false });
 
